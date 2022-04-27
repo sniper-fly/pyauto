a215ccb15e24d9c897e969f91643fbf8c2c8598a
rnakai <rnakai@student.42tokyo.jp>
2022-04-27 18:29:34 +0900

    kana自動切り替えのmouse verを作成

---
+++ toggle_kana_by_mouse.py
@@ -0,0 +1,19 @@
+import pyautogui
+import time
+
+top_bar_input_setting_coord = (2841, 89)
+mozc_coord = (2849, 183)
+input_mode_coord = (2824, 230)
+kana_mode_coord = (2834, 309)
+
+default_pause_time = 0.2
+
+def my_click(*coord, pause = default_pause_time):
+  time.sleep(pause)
+  pyautogui.click(*coord)
+
+my_click(*top_bar_input_setting_coord)
+my_click(*mozc_coord)
+my_click(*top_bar_input_setting_coord)
+my_click(*input_mode_coord)
+my_click(*kana_mode_coord)
