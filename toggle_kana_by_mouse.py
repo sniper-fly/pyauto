a041f6854750e91306cc66ba32b73b0634e1a40f
rnakai <rnakai@student.42tokyo.jp>
2022-04-27 18:53:30 +0900

    バッファ時間の調整

--- toggle_kana_by_mouse.py
+++ toggle_kana_by_mouse.py
@@ -14,9 +14,9 @@ def my_click(*coord, pause = default_pause_time):
 
 def main():
   beginning_position = pyautogui.position()
-  my_click(*top_bar_input_setting_coord, pause=0)
+  my_click(*top_bar_input_setting_coord)
   my_click(*mozc_coord)
-  my_click(*top_bar_input_setting_coord, pause=0)
+  my_click(*top_bar_input_setting_coord)
   my_click(*input_mode_coord)
   my_click(*kana_mode_coord)
   pyautogui.moveTo(*beginning_position)
