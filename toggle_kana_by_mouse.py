d3f0b0ea8025dd1d01737cf9aeb51360b0063e49
rnakai <rnakai@student.42tokyo.jp>
2022-08-14 09:37:55 +0900

    ディスプレイ設定変更による座標変更

--- toggle_kana_by_mouse.py
+++ toggle_kana_by_mouse.py
@@ -1,10 +1,10 @@
 import pyautogui
 import time
 
-top_bar_input_setting_coord = (2841, 89)
-mozc_coord = (2849, 183)
-input_mode_coord = (2824, 230)
-kana_mode_coord = (2834, 309)
+top_bar_input_setting_coord = (2847, 12)
+mozc_coord = (2847, 111)
+input_mode_coord = (2847, 163)
+kana_mode_coord = (2847, 234)
 
 default_pause_time = 0.1
 
